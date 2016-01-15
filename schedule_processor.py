866e07be4b77832009bb96f7da4045b554bff885
Grigori G <gr.gustin@gmail.com>
2016-01-15 13:32:53 +0200

    Refactored in two daemons

--- schedule_processor.py
+++ schedule_processor.py
@@ -7,13 +7,15 @@ import pdb
 import RPi.GPIO as GPIO
 from python_daemon import Daemon
 
+BOILER_PIN = 23
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(24, GPIO.OUT)
 GPIO.setup(23, GPIO.OUT)
 GPIO.setup(22, GPIO.OUT)
 
-
 room_status = {}
+room_temp_series = {}
 
 def getTime(time_str):
     r = int(time_str.split(':')[0])*60
@@ -22,49 +24,39 @@ def getTime(time_str):
 
 def getKey(entry):
     return getTime(entry['start'])
- 
-def get_required_temperature(room_id):
-    with open('/home/pi/project/schedule_{}.json'.format(room_id)) as data_file:    
-        data = json.load(data_file)
-        entry_list = sorted(data['schedule'],key=getKey)
-        time_list = []
-        current_time = time.localtime()
-        for i in entry_list:
-            minutes = current_time.tm_hour*60 + current_time.tm_min
-            if minutes >= getTime(i['start']) and minutes <= getTime(i['end']):
-                return (float(i['t']), float(i['th']))
-        # throw ex
 
-def update_status(ident, name, req_temp, current_temp):
-    json_res = {}
-    json_res['room_name']     = name
-    json_res['room_temp']     = current_temp[0]
-    json_res['room_ht']       = current_temp[1]
-    json_res['room_req_temp'] = req_temp[0]
-    json_res['room_req_htr']  = req_temp[1]
-    fh = open('/home/pi/project/status_{}.json'.format(ident), 'w')
-    fh.write(json.dumps(json_res))
-    fh.close()
 
 def get_current_temperature(ident):
-    logger = logging.getLogger('sp_logger')
-    con = sqlite3.connect('/home/pi/project/ess2.db')
-    con.row_factory = sqlite3.Row
-    cur = con.cursor()
-    ex = cur.execute('select * from stats order by time desc limit 5')
-    r = ex.fetchall()
-    return r
+    with open('/home/pi/project/status_{}.json'.format(ident)) as data_file:
+        data = json.load(data_file)
+        return data['room_temp'], data['room_ht']
+
+def get_required_temperature(ident):
+    with open('/home/pi/project/status_{}.json'.format(ident)) as data_file:
+        data = json.load(data_file)
+        return data['room_req_temp'], data['room_req_htr']
+
+def add_temperature_to_series(ident, temp):
+    if ident in room_temp_series:
+        room_temp_series[ident].append(temp)
+    else:
+        room_temp_series[ident] = [temp]
+    if len(room_temp_series[ident]) > 10:
+        room_temp_series[ident].pop(0)
 
 def is_heating_necessary(ident, name):
     required_temp = get_required_temperature(ident)
-    r = get_current_temperature(ident)
-    update_status(ident, name, required_temp, (r[0]['temp_room'], r[0]['temp_htr']))
+    current_temp = get_current_temperature(ident)
+    add_temperature_to_series(ident, current_temp)
+    r = room_temp_series[ident]
+    logger = logging.getLogger('sp_logger')
+    logger.warning('series is {}'.format(r))
     logger.warning('Required temperature is {}'.format(required_temp))
     for temp in r:
-        if temp['temp_room'] > required_temp[0]:
+        if temp[0] > required_temp[0]:
             logger.warning('Temperature in {} is more than req'.format(temp))
             return False
-        if temp['temp_htr'] is not None and temp['temp_htr'] > required_temp[1]:
+        if temp[1] is not None and temp[1] > required_temp[1]:
             logger.warning('Heater temperature in {} is greater than threshold'.format(temp))
             return False
     return True
@@ -77,32 +69,30 @@ def set_valves_state(room_status):
 def check_rooms(config_name, last_boiler_working_time, room_status):
     turn_boiler_on = False
     logger = logging.getLogger('sp_logger')
-    with open(config_name) as data_file:    
+    with open(config_name) as data_file:
         data = json.load(data_file)
         for room in data['room_mapping']:
-            name = room['name']
+            name  = room['name']
             ident = room['id']
-            pin = room['pin']
+            pin   = room['pin']
             if is_heating_necessary(ident, name) == True:
-#                GPIO.output(pin, 1)
                 turn_boiler_on = True
                 last_boiler_working_time = time.time()
                 if pin not in room_status or room_status[pin] == 0:
                     logger.warning(u'Turn ON heating in room {}, {}'.format(ident,name))
                     room_status[pin] = 1
             else:
-#                GPIO.output(pin, 0k)
                 if pin not in room_status or room_status[pin] == 1:
                     logger.warning(u'Turn OFF heating in room {}, {}'.format(ident,name))
                     last_boiler_working_time = time.time()
                     room_status[pin] = 0
         logger.warning("Last boiler working time is {}".format(last_boiler_working_time))
-        if turn_boiler_on == True:  
+        if turn_boiler_on == True:
             logger.warning("Turn boiler on")
             set_valves_state(room_status)
-            GPIO.output(23, 1)
-        else: 
-            GPIO.output(23, 0)
+            GPIO.output(BOILER_PIN, 1)
+        else:
+            GPIO.output(BOILER_PIN, 0)
             logger.warning("Turn boiler off")
             if last_boiler_working_time + 60 < time.time():
                 logger.warning("Finishing rooms")
--- stats_collector.py
+++ stats_collector.py
@@ -2,6 +2,47 @@ import json
 from pprint import pprint
 import sqlite3
 import time
+import sys
+from python_daemon import Daemon
+
+def getTime(time_str):
+    r = int(time_str.split(':')[0])*60
+    r += int(time_str.split(':')[1])
+    return r
+
+def getKey(entry):
+    return getTime(entry['start'])
+
+def get_required_temperature(room_id):
+    with open('/home/pi/project/schedule_{}.json'.format(room_id)) as data_file:
+        data = json.load(data_file)
+        entry_list = sorted(data['schedule'],key=getKey)
+        time_list = []
+        current_time = time.localtime()
+        for i in entry_list:
+            minutes = current_time.tm_hour*60 + current_time.tm_min
+            if minutes >= getTime(i['start']) and minutes <= getTime(i['end']) and getTime(i['start']) <= getTime(i['end']):
+                return (float(i['t']), float(i['th']))
+            if minutes >= getTime(i['start']) and minutes <= getTime(i['end'])+24*60 and getTime(i['start']) > getTime(i['end']):
+                return (float(i['t']), float(i['th']))
+
+
+def store_statistics(ident, name, req_temp, current_temp):
+    s = u'INSERT INTO stats (id, room, time, temp_room, temp_htr, target_room, target_htr) VALUES(\'{}\',\'{}\',{}, {}, {}, {}, {})'.format(str(ident),name, int(time.time()), current_temp[0], current_temp[1], req_temp[0], req_temp[1])
+    cur.execute(s)
+    con.commit()
+    return True
+
+def update_status(ident, name, req_temp, current_temp):
+    json_res = {}
+    json_res['room_name']     = name
+    json_res['room_temp']     = current_temp[0]
+    json_res['room_ht']       = current_temp[1]
+    json_res['room_req_temp'] = req_temp[0]
+    json_res['room_req_htr']  = req_temp[1]
+    fh = open('/home/pi/project/status_{}.json'.format(ident), 'w')
+    fh.write(json.dumps(json_res))
+    fh.close()
 
 
 def read_temperature(path):
@@ -11,22 +52,40 @@ def read_temperature(path):
     return temp[0:2] + '.' + temp[2:-1]
 
 pt_templ = "/sys/bus/w1/devices/{}/w1_slave"
- 
+
 con = sqlite3.connect('/home/pi/project/ess2.db')
 cur = con.cursor()
 
-print cur.lastrowid
-with open('config.json') as data_file:    
-    data = json.load(data_file)
-    for room in data['room_mapping']:
-        name = room['name']
-        rs = room['room_sensor']
-        hs = room['heater_sensor']
-        t = read_temperature(pt_templ.format(rs))
-        t_h = read_temperature(pt_templ.format(hs))
-        s = 'INSERT INTO stats (room, time, temp_room, temp_htr) VALUES(\'{}\',{}, {}, {})'.format(name, int(time.time()), t, t_h)
-        print s
-        cur.execute(s)
-        con.commit()
+class StatsDaemon(Daemon):
+    def run(self):
+        while True:
+            with open('/home/pi/project/config.json') as data_file:
+                data = json.load(data_file)
+                for room in data['room_mapping']:
+                    name  = room['name']
+                    rs    = room['room_sensor']
+                    hs    = room['heater_sensor']
+                    ident = room['id']
+                    t = read_temperature(pt_templ.format(rs))
+                    t_h = read_temperature(pt_templ.format(hs))
+                    req = get_required_temperature(ident)
+                    store_statistics(ident, name, req, (t, t_h))
+                    update_status(ident, name, req, (t, t_h))
+            time.sleep(60)
 
-pprint(data)
+if __name__ == "__main__":
+        daemon = StatsDaemon('/tmp/stats.pid', stdout = '/dev/stdout', stderr = '/dev/stderr')
+        if len(sys.argv) == 2:
+                if 'start' == sys.argv[1]:
+                        daemon.start()
+                elif 'stop' == sys.argv[1]:
+                        daemon.stop()
+                elif 'restart' == sys.argv[1]:
+                        daemon.restart()
+                else:
+                        print "Unknown command"
+                        sys.exit(2)
+                sys.exit(0)
+        else:
+                print "usage: %s start|stop|restart" % sys.argv[0]
+                sys.exit(2)
